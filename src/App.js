0b1ca44587608891d2c402e4e8c60ad4ce6652e5
itay-cyber <itaykag@gmail.com>
2020-10-25 15:25:50 +0100

    Fixed Speech Synthesis Stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BobbyMouth from './components/BobbyMouth';
-import SpeechSynthesisSpeaker from './components/SpeechSynthesisSpeaker'
-
 const mainText = "hello, world!"
-function App() {
-  return (
 
-    <div>
-      <BobbyMouth></BobbyMouth>
-      <SpeechSynthesisSpeaker utteranceParam="Hello, World!"></SpeechSynthesisSpeaker>
-    </div>
-    
-    
+function SpeechSynthesis(props) {
+
+  return (
+    <button onClick={() => {
+      var utterance = new SpeechSynthesisUtterance(props.utterancent)
+      utterance.voice = speechSynthesis.getVoices()[3]
+      speechSynthesis.speak(utterance)
+    }}></button>
   );
+  
+
 }
 
-export default App;
+
+export default function App() {
+  return (
+    
+
+    <div>
+      <SpeechSynthesis utterancent="Hello and welcome to British Airways. Please enjoy our wonderful boarding song oaoaoaioaoooa. cicicici"></SpeechSynthesis>
+      <BobbyMouth></BobbyMouth>
+
+      
+    </div>
+    
+  );
+  
+}
+
+window.onload = () => {
+  for (var i = 0; i < document.getElementsByTagName("button").length; i++) {
+    document.getElementsByTagName("button")[i].click()
+  }
+}
